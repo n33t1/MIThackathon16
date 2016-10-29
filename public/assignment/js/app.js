3dc3e2f1aa6dc160fca4b9f19c2d0628984411e4
BeanRider <weng.jef@husky.neu.edu>
2016-10-29 18:50:27 -0400

    I removed all of your things

--- public/assignment/js/app.js
+++
@@ -1,8 +0,0 @@
-// root of client side app
-
-// IFFE: anything in the function does not affect global fields,
-// nor does the global fields affect the fields inside the function
-(function() {
-    angular
-        .module("WAMApp", ["ngRoute", "textAngular", "wamDirectives"]); // add js modules (directives) here
-})();
